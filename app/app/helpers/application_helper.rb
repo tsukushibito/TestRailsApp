aeca7b97cac5f4854a17171d04016e3ebca0c59e
vagrant <vagrant@localhost.localdomain>
2019-06-09 14:31:48 +0000

    Refactor titling

--- app/app/helpers/application_helper.rb
+++ app/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  # Return full title of the page
+  def full_title(page_title = '')
+    base_title = 'Ruby on Rails Tutorial Sample App'
+    if base_title.empty?
+      base_title
+    else
+      page_title + ' | ' + base_title
+    end
+  end
 end
